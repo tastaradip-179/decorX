da8711056c581cef116496d91a3d88768e4984fe
tastaradip-179 <shahidasharmeenbaby@gmail.com>
2023-08-26 00:34:46 +0600

    commits

---
+++ app/Http/Controllers/TodoController.php
@@ -0,0 +1,132 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Todo;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TodoController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $todos = Todo::all();
+        return response()->json([
+            'status' => 'success',
+            'todos' => $todos,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+
+        $todo = Todo::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'user_id' => Auth::user()->id
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo created successfully',
+            'todo' => $todo,
+        ]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Todo $todo)
+    {
+        return response()->json([
+            'status' => 'success',
+            'todo' => $todo,
+        ]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Todo $todo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Todo $todo)
+    { 
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+
+        $todo->title = $request->title;
+        $todo->description = $request->description;
+        $todo->user_id = Auth::user()->id;
+        $todo->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo updated successfully',
+            'todo' => $todo,
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Todo  $todo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo deleted successfully',
+            'todo' => $todo,
+        ]);
+    }
+}
